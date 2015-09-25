d861a5dde7bf038b7beecc161e39c7f11f27db34
Derek <Derek@192.168.200.128>
2015-09-24 17:03:31 -0700

    Created last/Welcome controller and added its functionality; added more comments to First controller

--- application/controllers/last/Welcome.php
+++ application/controllers/last/Welcome.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Our First page. Shows the first author (picture, name and quote).
+ * Our Last page. Shows the last author (picture, name and quote).
  * 
- * controllers/First.php
+ * controllers/last/Welcome.php
  *
  * ------------------------------------------------------------------------
  */
-class First extends Application {
+class Welcome extends Application {
 
     function __construct() {
         parent::__construct();
@@ -17,7 +17,7 @@ class First extends Application {
         $this->data['pagebody'] = 'justone';    // this is the view we want shown
         // build the author with quote, to pass on to our view
         
-        $record = $this->quotes->first();
+        $record = $this->quotes->last();
         $author = array('who' => $record['who'], 'mug' => $record['mug'], 'what' => $record['what']);
 
         $this->data = array_merge($this->data, $author);
